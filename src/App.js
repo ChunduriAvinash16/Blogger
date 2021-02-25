b025ed51594b0fe0fdbf64e5a982c83ce51ac5d7
avinash <chunduriavinash6@gmail.com>
2021-02-25 10:30:21 +0530

    Intial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Blog from './Components/Blog';
+function App() {
+  return (
+    <Blog/>
+  );
+}
+
+export default App;
---
+++ src/Components/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Blog extends React.Component{
+    render(){
+    return(
+        <header>Hello</header>
+        
+    )
+    }
+}
+
+export default Blog;
